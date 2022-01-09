5724a5d7cda03b2be5713e8ff183006f90b5a3a0
Prateek Mittal <prateekmittal154@gmail.com>
2022-01-09 16:25:00 +0530

    Create power-of-2.cpp

---
+++ power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        int i;
+        
+        for(int i=0; i<31; i++){
+            
+            int ans= pow(2,i);
+            
+            if(ans==n){
+                
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
