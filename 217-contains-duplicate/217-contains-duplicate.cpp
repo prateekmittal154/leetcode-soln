30519097645b15535f4125c989136edace358b8d
Prateek Mittal <prateekmittal154@gmail.com>
2022-04-11 21:51:23 +0530

    Time: 117 ms (60.60%), Space: 51.5 MB (50.22%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,int> m;
+        
+        int n= nums.size();
+        
+        for(int i=0; i<n; i++){
+            
+           
+            if(m[nums[i]]>0) return true;
+            
+            m[nums[i]]++;
+        }
+        
+        return false;
+        
+    }
+};
