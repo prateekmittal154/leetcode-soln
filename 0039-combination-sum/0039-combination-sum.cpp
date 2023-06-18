e68574757223270b47ad2b94959e6b224cb2ec14
Prateek Mittal <prateekmittal154@gmail.com>
2023-06-18 14:14:19 +0530

    Time: 8 ms (61.00%), Space: 10.7 MB (87.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     
 void solve(vector<int>& candidates, int target, int index, vector<int>& ds, vector<vector<int>> &ans){
+    
+    if(index==candidates.size()){
+        if(target==0) {
+            ans.push_back(ds);
+        }
         
-        if(index == candidates.size()){
-            if(target==0) ans.push_back(ds);
-            return;
-        }
+        return;
+    }
+        
+    if(candidates[index]<= target){
+        ds.push_back(candidates[index]);
+        solve(candidates, target-candidates[index], index, ds, ans);
+        ds.pop_back();
+    }
     
-        if(candidates[index] <= target){
-            ds.push_back(candidates[index]);
-            solve(candidates,target -candidates[index],index,ds,ans);
-            ds.pop_back();
-        }
-    
-        solve(candidates, target, index+1 ,ds,ans);
+    solve(candidates, target, index+1,ds,ans);
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
